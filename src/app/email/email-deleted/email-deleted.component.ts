c27eb05abc734b7989004340ca16984ab5ca1290
Shahmir Asif <asifsh@deakin.edu.au>
2020-05-12 01:20:55 +1000

    delete function
    still adding delete functionality to the branch

--- src/app/email/email-deleted/email-deleted.component.ts
+++ src/app/email/email-deleted/email-deleted.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Email } from "../../models/email";
+import { EmailService } from "../../services/email.service";
+import { appUser as User } from "../../models/user";
 
 @Component({
-  selector: 'app-email-deleted',
-  templateUrl: './email-deleted.component.html',
-  styleUrls: ['./email-deleted.component.scss']
+  selector: "app-email-deleted",
+  templateUrl: "./email-deleted.component.html",
+  styleUrls: ["./email-deleted.component.scss"],
 })
 export class EmailDeletedComponent implements OnInit {
+  emails: Email[];
+  @Input() user: User;
 
-  constructor() { }
+  constructor(private emailService: EmailService) {}
 
   ngOnInit() {
+    this.emailService.delete(this.user).subscribe((emails) => {
+      this.emails = emails;
+    });
   }
-
 }
