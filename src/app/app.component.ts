21731aa722e194478daeff01a73e36c6d10a77fa
Kanishka Ratnayake <pratnayake@deakin.edu.au>
2020-05-15 11:48:43 +1000

    Automatically joins chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestoreCollection } from '@angular/fire/firestore';
 import { appUser } from './models/user';
 import { ChatAdapter } from 'ng-chat';
 import { SocketIOAdapter } from './services/socketio-adapter'
 import { Socket } from 'ng-socket-io';
 import { Http } from '@angular/http';
+import { Participant } from './models/participant';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'meps';
 
   userId: string;
   username: string;
-
+  user: Participant = { rolePosition: ""};
   public adapter: ChatAdapter;
 
-  constructor(private socket: Socket, private http: Http) {
-    this.InitializeSocketListerners();  
+  constructor(private socket: Socket, private http: Http, private auth: AuthService) {
+    this.InitializeSocketListerners(); 
   }
 
-  public joinRoom(): void 
+  ngOnInit()
   {
-    this.socket.emit("join", this.username);
+    this.auth.getUserData().subscribe(user => {
+      if(user === null) {
+      } else {
+          this.user = user[0];
+          this.username = this.user.firstName + " " + this.user.role ;
+          this.socket.emit("join", this.username);
+      }
+    })
   }
 
+ 
   public InitializeSocketListerners(): void
   {
     this.socket.on("generatedUserId", (userId) => {
