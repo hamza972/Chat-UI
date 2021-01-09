b640a16f78a7cc6dd9ee87e45f9ee0509628ff10
boolboolbool <bluefire6@live.com>
2021-01-09 21:16:19 +1100

    feat: add image resize option through custom ckeditor5 build.
    base64upload is included in custom ckeditor

--- src/app/email/email-compose/email-compose.component.ts
+++ src/app/email/email-compose/email-compose.component.ts
@@ -1,33 +1,47 @@
-import { Component, OnInit, Input } from "@angular/core";
-import * as Editor from "@ckeditor/ckeditor5-build-classic";
-import Base64Plugin from "./Base64Upload.js";
-import { ParticipantService } from "../../services/participant.service";
-import { Observable } from "rxjs";
-import { appUser as User } from "../../models/user";
-import { debounceTime, distinctUntilChanged, map } from "rxjs/operators";
-import { Email } from "src/app/models/email";
-import { EmailService } from "../../services/email.service";
+import { Component, OnInit, Input } from '@angular/core';
+import * as Editor from '../../../assets/custom-ckeditor/ckeditor';
+import { ParticipantService } from '../../services/participant.service';
+import { Observable } from 'rxjs';
+import { appUser as User } from '../../models/user';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { Email } from 'src/app/models/email';
+import { EmailService } from '../../services/email.service';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: "app-email-compose",
-  templateUrl: "./email-compose.component.html",
-  styleUrls: ["./email-compose.component.scss"],
+  selector: 'app-email-compose',
+  templateUrl: './email-compose.component.html',
+  styleUrls: ['./email-compose.component.scss'],
 })
 export class EmailComposeComponent implements OnInit {
   public Editor = Editor;
-  editorConfig =  {extraPlugins: [Base64Plugin]};
+  editorConfig = {
+    toolbar: {
+      items: [
+        'bold', 'italic', 'underline', 'link', 'bulletedList', 'numberedList',
+        '|', 'indent', 'outdent', '|', 'blockQuote', 'imageUpload', 'mediaEmbed', 'undo', 'redo',]
+    },
+    image: {
+      toolbar: [
+        'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
+        '|',
+        'imageTextAlternative'],
+      styles: [
+        'alignLeft', 'alignCenter', 'alignRight'],
+    },
+    language: 'en'
+  };
   participants: string[];
-  sendTo: string = "";
-  subject: string = "";
-  body: string = "";
+  sendTo = '';
+  subject = '';
+  body = '';
   @Input() user: User;
   newEmail: Email;
 
   constructor(
     private participantService: ParticipantService,
     private emailService: EmailService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.participantService.get().subscribe((participants) => {
@@ -35,7 +49,7 @@ export class EmailComposeComponent implements OnInit {
     });
   }
 
-  searchUsers = (search$: Observable<String>) =>
+  searchUsers = (search$: Observable<string>) =>
     search$.pipe(
       debounceTime(200),
       distinctUntilChanged(),
@@ -43,13 +57,13 @@ export class EmailComposeComponent implements OnInit {
         query.length < 2
           ? []
           : this.participants
-              .filter(
-                (participant) =>
-                  participant.toLowerCase().indexOf(query.toLowerCase()) > -1
-              )
-              .slice(0, 10)
+            .filter(
+              (participant) =>
+                participant.toLowerCase().indexOf(query.toLowerCase()) > -1
+            )
+            .slice(0, 10)
       )
-    );
+    )
 
   send(frm: NgForm) {
     this.newEmail = {
@@ -64,9 +78,7 @@ export class EmailComposeComponent implements OnInit {
     };
     frm.reset();
     this.emailService.sendEmail(this.newEmail);
-    console.log("sending");
-    alert("Your Email has been sent!!");
-    //var form = <HTMLInputElement>document.getElementById("Form").reset();
+    alert('Your Email has been sent!!');
   }
 
   draft(frm: NgForm) {
@@ -74,15 +86,14 @@ export class EmailComposeComponent implements OnInit {
       subject: this.subject,
       body: this.body,
       to: {
-        user: this.sendTo || " ",
+        user: this.sendTo || ' ',
       },
       from: {
         user: this.user.email,
       },
     };
     this.emailService.draftEmail(this.newEmail);
-    console.log("drafting");
-    alert("Your Email has been been moved to the drafts!!");
+    alert('Your Email has been been moved to the drafts!!');
     frm.reset();
   }
 }
