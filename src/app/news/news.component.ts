a8ffa4c0f4612e89fdb255c2615d54f51da59298
asifsh <shahmirghurki@gmail.com>
2021-01-28 01:44:47 +1100

    News UI improvements. -Added an alert box to notify users on successful posting of news. -Improved the overall layout of the page.

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -7,11 +7,12 @@ import { News } from '../models/News';
 import { Participant } from '../models/participant';
 import { AuthService } from '../services/auth.service';
 import * as Editor from '../../assets/custom-ckeditor/ckeditor';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  styleUrls: ['./news.component.scss'],
 })
 export class NewsComponent implements OnInit {
   public Editor = Editor;
@@ -45,7 +46,9 @@ export class NewsComponent implements OnInit {
   constructor(
     private auth: LoginService,
     private auth2: AuthService,
-    private router: Router ) { }
+    private router: Router,
+    private modalService: NgbModal) { }
+
 
   ngOnInit() {
     /* Check if user is signed in, otherwise redirect to home */
@@ -68,15 +71,18 @@ export class NewsComponent implements OnInit {
   }
 
 
-  createNews(frm, frm2) {
+  createNews(frm, frm2, content) {
     this.newUserNews = {
       userName: this.newsUser.firstName + ' ' + this.newsUser.lastName,
       newsDate: new Date(),
       newsDescription: frm.value,
       newsHeadline: frm2.value,
       userEmail: this.newsUser.email,
-      userRole: this.newsUser.role };
-  }
+      userRole: this.newsUser.role 
+  };
+  this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => { });
+
+}
 
   btnClick = function() {
     this.router.navigateByUrl('/news-publish');
